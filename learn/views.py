1993eeffb861211c2cddf989e1e7115d12f90e8a
oliLamRou <parties01smacks@icloud.com>
2025-04-04 19:07:47 -0400

    fancy random based on learning

--- learn/views.py
+++ learn/views.py
@@ -1,11 +1,45 @@
+import random
+import math
 from django.http import HttpResponse, JsonResponse
+from django.db.models import F
 from django.shortcuts import render, get_object_or_404
 
 from dictionary.models import Word
 from learn.models import Score
 
+def get_some_querySet(querySet, perc=1):
+    queryList = list(querySet)
+    queryListLen = len(queryList)
+    random.shuffle(queryList)
+    amount = math.ceil(queryListLen * perc)
+    return queryList[:amount]
+
 def guess(request):
-    word = Word.objects.order_by("?").first() #this is bad
+    #Get all words that never been seen
+    #Get 1% of word that are good > bad
+    #Get 5% of word that are bad > good
+    #Get 10% of last_try > 1 month
+
+    #Choose 1 word in those above
+
+    #New
+    words_without_score = Word.objects.filter(score__isnull=True).order_by("?")
+    
+    #Score
+    words_with_score = Word.objects.filter(score__isnull=False)
+    words_good = words_with_score.filter(score__success__gt=F('score__fail'))
+    words_bad = words_with_score.filter(score__success__lte=F('score__fail'))
+
+    #Get all or some query
+    words_without_score_list = get_some_querySet(words_without_score)
+    words_good_list = get_some_querySet(words_good, 0.01)
+    words_bad_list = get_some_querySet(words_bad, 0.05)
+
+    #Combien and shuffle
+    word_list = words_without_score_list + words_good_list + words_bad_list
+    random.shuffle(word_list)
+    word = word_list[-1]
+
     print(f'Guess New Word: {word.french}')
 
     return render(request, "guess.html", {"word": word})

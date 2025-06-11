8e5b39a34345a777fca9cf9032b6b926822ed4d9
oliLamRou <parties01smacks@icloud.com>
2025-06-11 17:12:04 +0900

    initial commit

--- frontend/src/routes/auth/credentials/+page.svelte
+++ frontend/src/routes/auth/credentials/+page.svelte
@@ -1,47 +1,51 @@
 <script>
-    import { goto } from "$app/navigation";
-	import { api } from "$lib/api/api";
-	import { _login, _refresh } from "$lib/api/auth";
-	import { userState } from "$lib/state.svelte";
-	import { onMount } from "svelte";
-    
-    let username = $state('')
-    let password = $state('')
-    let authType = $state(true)
+	import { goto } from '$app/navigation';
+	import { api } from '$lib/api/api';
+	import { _login, _refresh } from '$lib/api/auth';
+	import { userState } from '$lib/state.svelte';
+	import { onMount } from 'svelte';
 
-    onMount(() => {
-        if (localStorage.getItem('REFRESH_TOKEN_KEY')) {
-            console.info("User Return, Trying redirect to /learn")
-            goto('/app/learn')
-        }
-    })
+	let username = $state('');
+	let password = $state('');
+	let authType = $state(true);
+	let waiting = $state(false);
 
-    const login = async() => {
-        const response = await _login(username, password)
-        if (response) {
-            goto('/app/learn')
-        }
-    }
+	onMount(() => {
+		if (localStorage.getItem('REFRESH_TOKEN_KEY')) {
+			console.info('User Return, Trying redirect to /learn');
+			goto('/app/learn');
+		}
+	});
 
-    const signUp = () => {
-        goto('/auth/register')
-    }
-    
+	const login = async () => {
+		if (waiting) return;
+		waiting = true;
+		const response = await _login(username, password);
+		if (response) {
+			waiting = false;
+			goto('/app/learn');
+		}
+	};
+
+	const signUp = () => {
+		goto('/auth/register');
+	};
 </script>
 
 <div class="min-h-screen flex items-center justify-center">
-    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <form on:submit|preventDefault={login}>
-            <legend class="fieldset-legend">Welcome Back!</legend>
-        
-            <label class="label">Username</label>
-            <input type="username" class="input validator" required bind:value={username}/>
-        
-            <label class="label">Password</label>
-            <input type="password" class="input validator" required bind:value={password}/>
-        
-            <button class="btn btn-block btn-primary mt-4" type="submit">Login</button>
-        </form>    
-        <a class="link justify-center" on:click={signUp}>Create New Account here!</a>
-    </fieldset>
+	<fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+		<form on:submit|preventDefault={login}>
+			<legend class="fieldset-legend">Welcome Back!</legend>
+
+			<label class="label">Username</label>
+			<input type="username" class="input validator" required bind:value={username} />
+
+			<label class="label">Password</label>
+			<input type="password" class="input validator" required bind:value={password} />
+
+			<button class="btn btn-block btn-primary mt-4" type="submit">Login</button>
+		</form>
+		<a class="link justify-center" on:click={signUp}>Create New Account here!</a>
+	</fieldset>
 </div>
+

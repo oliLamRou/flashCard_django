825f3ba074de685f42145ffec09c2d9ade13f903
oliLamRou <parties01smacks@icloud.com>
2025-06-12 13:57:53 +0900

    indent better

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,20 +1,20 @@
 <script>
-    import "../app.css";
-	import { onMount } from "svelte";
-	import { goto } from "$app/navigation";
-	import { appState, userState } from "$lib/state.svelte";
-    
-    let { children } = $props();
+	import '../app.css';
+	import { onMount } from 'svelte';
+	import { goto } from '$app/navigation';
+	import { appState, userState } from '$lib/state.svelte';
 
-    onMount(() => {
-        if (localStorage.getItem('REFRESH_TOKEN_KEY')) {
-            console.info("User Return, Trying redirect to /learn")
-            goto('/app/learn')
-        } else {
-            goto('/auth/credentials')
-        }
-    })
+	let { children } = $props();
 
+	onMount(() => {
+		if (localStorage.getItem('REFRESH_TOKEN_KEY')) {
+			console.info('User Return, Trying redirect to /learn');
+			goto('/app/learn');
+		} else {
+			goto('/auth/credentials');
+		}
+	});
 </script>
 
 {@render children()}
+
